dea66515c55f35b57996ac191e7034dc920f1e9f
Faiza RABHI <93930498+Faizarabhi@users.noreply.github.com>
2024-01-12 21:19:29 +0100

    feat : update

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,11 +7,12 @@
  * @argv: argument vector
  * Return: 0 if no errors
  */
-int main(int argc, char *argv[])
-{
-	int i;
 
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
+
+int main (int argc,char *argv[]){
+	int i;
+	for(i = 0; i< argc; i++){
+		printf("%s\n",argv[i]);
+	}
 	return (0);
 }
