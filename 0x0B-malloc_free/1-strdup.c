da6e92e6630d74afed1deafef5714719b793f8ba
Faiza RABHI <93930498+Faizarabhi@users.noreply.github.com>
2024-01-18 01:09:23 +0100

    fix betty

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,19 +6,19 @@
  */
 char *_strdup(char *str)
 {
-	int leng = 0, i;
-	char *ptr;
+int leng = 0, i;
+char *ptr;
 
-	if (str == NULL)
-		return (NULL);
-	for (; str[leng] != '\0'; leng++)
-		;
-	ptr = malloc((leng + 1) * sizeof(char));
-	if (ptr == NULL)
-		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		ptr[i] = str[i];
-	}
-	return (ptr);
+if (str == NULL)
+return (NULL);
+for (; str[leng] != '\0'; leng++)
+;
+ptr = malloc((leng + 1) * sizeof(char));
+if (ptr == NULL)
+return (NULL);
+for (i = 0; str[i] != '\0'; i++)
+{
+ptr[i] = str[i];
+}
+return (ptr);
 }
