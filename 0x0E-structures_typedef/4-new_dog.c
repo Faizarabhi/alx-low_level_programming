fd1715b01840d24f854e496a1ac14512af2906ef
Faiza RABHI <93930498+Faizarabhi@users.noreply.github.com>
2024-01-23 01:49:58 +0100

    fix task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,18 +1,39 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
 /**
- * new_dog - function
- * @name: the par
- * @age: the par
- * @owner: the par
- * Return: pointer wich point on qtruct "dog_t"
+ * new_dog - function to create a new dog
+ * @name: pointer to the name of the dog
+ * @age: age of the dog
+ * @owner: pointer to the owner of the dog
+ * Return: pointer to the new dog structure
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t new_dog = {name, age, owner};
+    dog_t *new_dog;
 
-	if (new_dog == NULL)
-		return (NULL);
-	return (new_dog);
+    new_dog = malloc(sizeof(dog_t));
+    if (new_dog == NULL)
+        return (NULL);
+
+    new_dog->name = strdup(name);
+    if (new_dog->name == NULL)
+    {
+        free(new_dog);
+        return (NULL);
+    }
+
+    new_dog->age = age;
+
+    new_dog->owner = strdup(owner);
+    if (new_dog->owner == NULL)
+    {
+        free(new_dog->name);
+        free(new_dog);
+        return (NULL);
+    }
+
+    return (new_dog);
 }
